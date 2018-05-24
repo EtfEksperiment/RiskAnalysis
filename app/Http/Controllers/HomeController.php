0eed66fb36a34fbf4a2c63e9c9db09560268cc26
Suki <sukibataja@gmail.com>
2018-05-24 16:55:39 +0200

    download txt

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,8 +29,9 @@ class HomeController extends Controller
     public function getPdf() {
         $parser = new \Smalot\PdfParser\Parser();
         $pdf    = $parser->parseFile(public_path('bil.pdf'));
- 
-        $text = $pdf->getText();
-        echo $text;
+        
+        header("Content-Type: text/text; charset=UTF-8");
+        header('Content-disposition: attachment;filename="pdf.txt"'); 
+        echo $pdf->getText();
     }
 }
